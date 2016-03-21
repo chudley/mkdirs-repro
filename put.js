d79a9ae0af7fdac00a933d5a52634fc71b29da59
richard <richard.bradley@joyent.com>
2016-03-21 16:39:58 +0000

    Initial commit

---
+++ put.js
@@ -0,0 +1,42 @@
+var assert = require('assert');
+var fs = require('fs');
+
+var manta = require('manta');
+var crypto = require('crypto');
+var MemoryStream = require('memorystream');
+var bunyan = require('bunyan');
+
+var log = bunyan.createLogger({
+    name: 'mkdirs',
+    stream: process.stdout,
+    level: 'trace'
+});
+
+var client = manta.createClient({
+    sign: manta.privateKeySigner({
+        key: fs.readFileSync(process.env.HOME + '/.ssh/id_rsa', 'utf8'),
+        keyId: process.env.MANTA_KEY_ID,
+        user: process.env.MANTA_USER
+    }),
+    user: process.env.MANTA_USER,
+    url: process.env.MANTA_URL,
+    log: log
+});
+log.info('manta ready: %s', client.toString());
+
+var message = 'mkdirs\n';
+var opts = {
+    md5: crypto.createHash('md5').update(message).digest('base64'),
+    size: Buffer.byteLength(message),
+    type: 'text/plain',
+    mkdirs: true
+};
+var read_stream = new MemoryStream();
+
+client.put('~~/stor/mkdirs/file.txt', read_stream, opts, function (err) {
+    assert.ifError(err);
+    log.info('done');
+    client.close();
+})
+
+read_stream.end(message);
